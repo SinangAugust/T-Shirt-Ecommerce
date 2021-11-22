0186049f342297d3aa60e45a1ba9d8e0d75f5163
August <august.sinang@gmail.com>
2021-11-22 19:09:03 +0800

    Setting Up Categories in Admin

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from django.urls.resolvers import URLPattern
 from rest_framework.authtoken import views
 from .views import home
 
 urlpatterns = [
-    path('', home, name="api.home")
+    path('', home, name="api.home"),
+    path('category/', include('api.category.urls'))
 ]
