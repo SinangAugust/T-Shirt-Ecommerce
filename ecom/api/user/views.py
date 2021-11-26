fd902b7dab05eeb81b2bb69113a5f5dc72333ed1
August <august.sinang@gmail.com>
2021-11-26 23:55:29 +0800

    Setting Up Custom User in Admin: 4-6

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permission import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
+import re
 
+import random
 # Create your views here.
+
+
+def generate_session_token(length=10):
+    # auth token generator
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
+
+# request from other site?
+@csrf_exempt
+def signin(request):
+    if not request.method == 'POST':
+        return JsonResponse({'error': 'Send a post request with valid parameter only'})
+
+    username = request.POST['email']
+    password = request.POST['password']
+
+    # validation
+    if not re.match("(?!.*[\.\-\_]{2,})^[a-zA-Z0-9\.\-\_]{3,24}$", username):
+        return JsonResponse({'error': 'Enter a valid email'})
+
+    if len(password) < 8:
+        return JsonResponse({'error': 'Password needs at least 8 or more characters'})
+
+    UserModel = get_user_model()
+
+
+    try:
+        user = UserModel.objects.get(email=username)
+
+        if user.check_password(password):
+            usr_dict = UserModel.objects.filter(email=username).values().first()
+            usr_dict.pop('password')
+
+            if user.session_token != "0":
+                user.session_token = "0"
+                user.save()
+                return JsonResponse({'error': 'Previous session exists!'})
+
+            token = generate_session_token()
+            user.session_token = token
+            user.save()
+            login(request, user)
+            return JsonResponse({'token': token, 'user': usr_dict})
+            
+        else:
+            return JsonResponse({'error': 'Invalid password'})
+
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error': 'Invalid email'})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes_by_action = {'create': [AllowAny]}
+
+    queryset = CustomUser.objects.all().order_by('id')
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
