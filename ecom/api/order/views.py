ae812174caacbc50d79984482fea50b47c19867b
August <august.sinang@gmail.com>
2021-12-04 22:00:15 +0800

    Getting Orders in Admin Django: 4-5 ; With bug in urls

--- ecom/api/order/views.py
+++ ecom/api/order/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.backends import UserModel
 from rest_framework import viewsets
 from django.http import JsonResponse
-from django.contrib.auth import get_user_model
-from .serializers import serializers
+from django.contrib.auth import get_user, get_user_model
+from .serializers import OrderSerializer
 from .models import Order
 from django.views.decorators.csrf import csrf_exempt
 
@@ -18,3 +18,33 @@ def validate_user_session(id, token):
         
     except UserModel.DoesNotExist:
         return False
+
+
+@csrf_exempt
+def add(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Please login again', 'code': '1'})
+
+    if request.method == "POST":
+        user_id = id
+        transaction_id = request.POST['transaction_id']
+        amount = request.POST['amount']
+        products = request.POST['products']
+
+        total_product = len(products.split(',')[:-1])
+
+        UserModel  = get_user_model()
+
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return JsonResponse({'error': 'User does not exist'})
+
+        ordr = Order(user=user, product_names=products, total_products=total_product, transaction_id=transaction_id, total_amount=amount)
+        ordr.save()
+        return JsonResponse({'success': True, 'error': False, 'msg': 'Order placed successfully'})
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('name')
+    serializer_class = OrderSerializer
