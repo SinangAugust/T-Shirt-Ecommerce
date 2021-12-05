f7dff66f3efad6d6e831b405c4bef465b75967c4
August <august.sinang@gmail.com>
2021-12-05 21:38:30 +0800

    Payment gateway backend: Generate braintree token for user in Django 2

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.backends import UserModel
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+import braintree
 
 # Create your views here.
+
+
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        braintree.Environment.Sandbox,
+        merchant_id="ph8gsfp8zpqgx2yg",
+        public_key="dr3xhtts7npjwhgq",
+        private_key="e090f77983be366c6872ebb670193a3c"
+    )
+)
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
+
+
+
+@csrf_exempt
+def generate_token(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, please login again'})
+
+    return JsonResponse({'clientToken': gateway.client_token.generate(), 'success': True})
