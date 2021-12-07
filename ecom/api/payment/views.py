4f89163678015b834c7942bb2a53e0c0d54e127d
August <august.sinang@gmail.com>
2021-12-08 00:01:44 +0800

    Payment gateway backend: Process the payment from backend in django

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.views.decorators import csrf
 from django.views.decorators.csrf import csrf_exempt
 import braintree
 
@@ -37,3 +38,32 @@ def generate_token(request, id, token):
         return JsonResponse({'error': 'Invalid session, please login again'})
 
     return JsonResponse({'clientToken': gateway.client_token.generate(), 'success': True})
+
+
+@csrf_exempt
+def process_payment(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, please login again'})
+
+    nonce_from_the_client = request.POST("paymentMethodNonce")
+    amount_from_the_client = request.POST("amount")
+
+    result = gateway.transaction.sale({
+        "amount": amount_from_the_client,
+        "payment_method_nonce": nonce_from_the_client,
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success:
+        return JsonResponse({
+            'success': result.is_success,
+            'transaction': {
+                'id': result.transaction.id,
+                'amount': result.transaction.amount
+            }
+        })
+
+    else:
+        return JsonResponse({'error': True, 'success': False})
