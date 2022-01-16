7558b6eba5b28793464d28fc740296defc79e1ee
august <august.sinang@gmail.com>
2022-01-16 21:20:36 +0800

    Fix all errors

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-import {getProducts} from "./helper/coreapicalls"
+import { getProducts } from "./helper/coreapicalls"
 import Base from './Base'
 
 import '../styles.css'
@@ -12,13 +12,15 @@ export default function Home() {
     const [error, setError] = useState(false)
 
     const loadAllProducts = () => {
-        getProducts().then(data =>{
+        getProducts()
+        .then(data => {
             if (data.error){
                 setError(data.error)
                 console.log(error)
             } else {
                 setProducts(data)
             }
+            // console.log(data)
         })
     }
 
