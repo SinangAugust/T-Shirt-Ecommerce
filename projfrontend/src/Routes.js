5c0d27759d8c14af8061ee12e98c59a09159be21
august <august.sinang@gmail.com>
2022-01-18 00:11:48 +0800

    Cart and Auth Helper: Handling Private Routes in React

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import PrivateRoutes from './auth/helper/PrivateRoutes'
 
 import Home from './core/Home'
+
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                {/* <PrivateRoutes path="/user/dashboard" exact component={}/> */}
             </Switch>
         </BrowserRouter>
     )
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -20,7 +20,6 @@ export default function Home() {
             } else {
                 setProducts(data)
             }
-            // console.log(data)
         })
     }
 
