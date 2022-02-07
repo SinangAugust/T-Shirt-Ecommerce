9b6c3ca9874dfc0977ebdd0118647fd6d784d13b
august <august.sinang@gmail.com>
2022-02-07 17:12:40 +0800

    Navigation Bar: Setting up the nav bar

---
+++ projfrontend/src/core/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div>
+            <ul className='nav nav-tabs bg-dark'>
+                <li className='nav-item'>
+                    <a className="nav-link" href='#'>HOME</a>
+                </li>
+                <li className='nav-item'>
+                    Signin
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Menu
